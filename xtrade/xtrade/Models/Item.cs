68b887cb81c3e9d781a2460943cb510d0b7cea57
kiranhsgithub <hskiran.bms@gmail.com>
2016-11-21 17:08:41 -0500

    added model Item

---
+++ xtrade/xtrade/Models/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace xtrade.Models
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [EmailAddress]
+        public string Seller { get; set; }
+
+        [EmailAddress]
+        public string Buyer { get; set; }
+
+        [Required]
+        public double amount { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set;  }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+
+
+
+
+
+    }
+}
