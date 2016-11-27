15caf268a6f5cc9a47e18e5176af8644d4081a35
kiranhsgithub <hskiran.bms@gmail.com>
2016-11-27 10:31:03 -0500

    added angular scripts

---
+++ xtrade/xtrade/Scripts/app/item-controller.js
@@ -0,0 +1,23 @@
+﻿app = angular.module('XTradeApp');
+app.controller('ItemController', ['$scope', function ($scope) {
+    $scope.images = [{
+        name: "UploadImage1"
+    }];
+
+
+    $scope.addImage = function () {
+
+        let length = $scope.images.length;
+        $scope.images.push({
+            name: "UploadImage" + length
+        });
+    };
+
+    $scope.removeImage = function (name) {
+        let index = _.findIndex($scope.images, {
+            name: name
+        });
+
+        $scope.images.splice(index, 1);
+    };
+}]);
