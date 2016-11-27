219aca63ef458d639fee2a7d4ee2f26fe9856ff4
ankitshukla- <ankitshukla34@gmail.com>
2016-11-26 21:19:58 -0500

    Commit 1

---
+++ xtrade/xtrade/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace xtrade.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
+
---
+++ xtrade/xtrade/Models/Item.cs
@@ -0,0 +1,41 @@
+﻿
+namespace xtrade.Models
+{
+    public enum Classification { Automotive, Furniture, ClothesNShoes, Electronics, Books, Services, Miscellaneous};
+    
+    public class Item
+    {
+        static int item_count = 1;
+
+        //User enters
+        public string name { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public Classification category { get; set; }
+        
+        //Set automatically
+        public int id { get; set; }
+        public bool bIsAvailable;
+        public User seller { get; set; }
+        public User buyer { get; set; }              
+        
+        public System.DateTime postedOn;
+
+        public int nViews;
+        public Item()
+        {
+            bIsAvailable = true;
+            nViews = 0;
+            postedOn = new System.DateTime();
+            postedOn = System.DateTime.Now;
+            id = item_count++;
+            //seller = currentUser; 
+        }
+
+        public void buy(ref User Buyer)
+        {
+            bIsAvailable = false;
+            //buyer = Buyer; 
+        }
+    }
+}
