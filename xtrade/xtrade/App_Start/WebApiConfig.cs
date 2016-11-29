464627233722c8d9a9ba8c218e853a943ead3e29
kiranhsgithub <hskiran.bms@gmail.com>
2016-11-29 18:35:41 -0500

    rest apis

--- xtrade/xtrade/App_Start/WebApiConfig.cs
+++ xtrade/xtrade/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace xtrade
@@ -19,6 +21,12 @@ namespace xtrade
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.JsonFormatter.SerializerSettings
+                .Re‌​ferenceLoopHandling = ReferenceLoopHandling.Ignore;
         }
     }
 }
